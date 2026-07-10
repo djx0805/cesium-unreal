b4dc624871fd9d5063484a437428d4fe1964eb95
Janine Liu <janine.liu@bentley.com>
2026-07-10 10:42:33 -0400

    Formatting

--- Source/CesiumRuntime/Private/CesiumGltfComponent.cpp
+++ Source/CesiumRuntime/Private/CesiumGltfComponent.cpp
@@ -2518,7 +2518,7 @@ loadModelAnyThreadPart(
   return CesiumGltfTextures::createInWorkerThread(asyncSystem, *options.pModel)
       .thenInWorkerThread(
           [transform, ellipsoid, options = std::move(options)]() mutable
-              -> UCesiumGltfComponent::CreateOffGameThreadResult {
+          -> UCesiumGltfComponent::CreateOffGameThreadResult {
             auto pHalf = MakeUnique<HalfConstructedReal>();
 
             loadModelMetadata(pHalf->loadModelResult, options);
